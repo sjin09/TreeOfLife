499ccf518c852d17fe472e102abc9eaf6b15aef1
sjin09 <sl17@sanger.ac.uk>
2025-01-30 12:28:06 +0900

    minor update

--- scripts/get_sample_taxonomic_tree.py
+++ scripts/get_sample_taxonomic_tree.py
@@ -4,8 +4,10 @@ import argparse
 from pathlib import Path
 import sys
 
-# from Bio import Phylo
 from ete3 import Tree
+import pandas as pd
+
+TAXONOMIC_RANKS = ['Kingdom', 'Phylum', 'Class', 'Order', 'Family', 'Genus', 'Species']
 
 
 def parse_args(args):
@@ -30,39 +32,6 @@ def parse_args(args):
     args = args[1:]
     return parser.parse_args(args)
 
-# Use Bio.Phylo and Bio.Phylo.Netwick
-# def write_sample_newick_tree(input_path: Path, output_path: Path) -> None:
-#     def get_newick_tree(nested_taxonomy: TaxonomicTree) -> str:
-#         if isinstance(nested_taxonomy, list):
-#             species = [f"'{element}'" for element in nested_taxonomy]
-#             return "(" + ",".join(species) + ")"
-#         if isinstance(nested_taxonomy, dict):
-#             return "(" + ",".join(f"{key}:{get_newick_tree(value)}" for key, value in nested_taxonomy.items()) + ")"
-#         return str(nested_taxonomy)
-#
-#     # Initialize
-#     nested_taxonomy = defaultdict(
-#         lambda: defaultdict(lambda: defaultdict(lambda: defaultdict(lambda: defaultdict(list))))
-#     )
-#
-#     # Import
-#     df = pd.read_csv(input_path)
-#     for _ridx, row in df.iterrows():
-#         species = row["Species"]
-#         kingdom = row["Kingdom"]
-#         phylum = row["Phylum"]
-#         subphylum = row["Subphylum"]
-#         class_taxon = row["Class"]
-#         order = row["Order"]
-#         if species not in nested_taxonomy[kingdom][phylum][subphylum][class_taxon][order]:
-#             nested_taxonomy[kingdom][phylum][subphylum][class_taxon][order].append(species)
-#
-#     # Reformat
-#     newick_tree = get_newick_tree(nested_taxonomy)
-#     tree = Phylo.read(StringIO(newick_tree), "newick")
-#     ## Phylo.draw_ascii(tree)
-#     Phylo.write(tree, output_path, "newick")
-
 
 # Use ete3
 def write_sample_taxonomic_tree(input_path: Path, output_path: Path) -> None:
@@ -79,24 +48,21 @@ def write_sample_taxonomic_tree(input_path: Path, output_path: Path) -> None:
     # Initialize tree
     root = Tree(name="root")
 
-    # Build tree based on taxonomic classifications per sample
-    with open(input_path) as infile:
-        for line in infile:
-            if line.startswith("Reference"):
+    # Import and iterate through the DToL samplesheet
+    df = pd.read_csv(input_path)
+    for (_idx, row) in df.iterrows():
+        current_node = root
+        sample_taxonomic_ranks = list(row[TAXONOMIC_RANKS])
+        for rank in sample_taxonomic_ranks:
+            if rank == ".":
                 continue
-            current_node = root
-            fields = line.rstrip().split(",")
-            taxonomic_ranks = fields[5:] + [fields[2]]
-            for rank in taxonomic_ranks:
-                if rank == ".":
-                    continue
-                current_node = get_child(current_node, rank)
+            current_node = get_child(current_node, rank)
 
     # Export tree to newick format
     root.write(format=1, outfile=str(output_path))
 
     # Show tree for debugging
-    # root.show()
+    root.show()
 
 
 def main() -> int:
--- scripts/get_taxonomic_tree.py
+++ scripts/get_taxonomic_tree.py
@@ -5,6 +5,10 @@ from pathlib import Path
 import sys
 
 from ete3 import Tree
+import pandas as pd
+
+
+TAXONOMIC_RANKS = ['Kingdom', 'Phylum', 'Class', 'Order']
 
 
 def parse_args(args):
@@ -29,30 +33,6 @@ def parse_args(args):
     args = args[1:]
     return parser.parse_args(args)
 
-## Use Bio.Phylo and Bio.Phylo.Netwick
-## def write_taxa_newick_tree(input_path: Path, output_path: Path) -> None:
-##     def get_child(parent_clade: Clade, child_name: str):
-##         for child in parent_clade.clades:
-##             if child.name == child_name:
-##                 return child
-##         new_child = Clade(name=child_name)
-##         parent_clade.clades.append(new_child)
-##         return new_child
-## 
-##     root = Clade(name="root")  # Root node for the tree
-##     for line in open(input_path):
-##         if line.startswith("Reference"):
-##             continue
-##         current_node = root
-##         taxa = line.rstrip().split(",")[5:]
-##         for rank in taxa:
-##             if rank == ".":
-##                 continue
-##             current_node = get_child(current_node, rank)
-##     tree = Tree(root=root, rooted=True)
-##     # Phylo.draw_ascii(tree)
-##     Phylo.write(tree, output_path, "newick")
-
 
 # Use ete3
 def write_taxonomic_tree(input_path: Path, output_path: Path) -> None:
@@ -69,23 +49,22 @@ def write_taxonomic_tree(input_path: Path, output_path: Path) -> None:
     # Initialize tree
     root = Tree(name="root")
 
-    # Build tree based on taxonomic classifications per sample
-    with open(input_path) as infile:
-        for line in infile:
-            if line.startswith("Reference"):
+    # Import and iterate through the DToL samplesheet
+    df = pd.read_csv(input_path)
+    for (_idx, row) in df.iterrows():
+        current_node = root
+        sample_taxonomic_ranks = list(row[TAXONOMIC_RANKS])
+        for rank in sample_taxonomic_ranks:
+            if rank == ".":
                 continue
-            current_node = root
-            taxonomic_ranks = line.rstrip().split(",")[5:]
-            for rank in taxonomic_ranks:
-                if rank == ".":
-                    continue
-                current_node = get_child(current_node, rank)
+            current_node = get_child(current_node, rank)
 
     # Export tree to newick format
     root.write(format=1, outfile=str(output_path))
 
     # Show tree for debugging
-    # root.show()
+    root.show()
+
 
 def main() -> int:
     options = parse_args(sys.argv)
